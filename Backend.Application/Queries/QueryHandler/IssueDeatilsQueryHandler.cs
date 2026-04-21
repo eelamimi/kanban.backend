33ff9947f1ca87a81f8e93d46344982524c0b8ab
glebka <b.gleba77@gmail.com>
2026-04-21 10:53:41 +0300

    todo: get all attachments

---
+++ Backend.Application/Queries/QueryHandler/IssueDeatilsQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace Backend.Application.Queries.QueryHandler;
+
+public class IssueDeatilsQueryHandler(
+    IIssueRepository issueRepository,
+    IAttachmentRepository attachmentRepository)
+    : ICommandHandler<IssueDeatilsQuery, IssueResponse>
+{
+    public async Task<IssueResponse> Handle(IssueDeatilsQuery command, CancellationToken token)
+    {
+        var numberInProject = int.Parse(command.PublicId.Split('-', 1)[1]);
+        var issue = await issueRepository.GetByNumberInProjectAndProjectIdsAsync(numberInProject, command.ProjectId, true, token);
+        var attachments = await attachmentRepository.GetAllByIssueIdAsync(issue.Id, token);
+
+        return issue.Map();
+    }
+}
