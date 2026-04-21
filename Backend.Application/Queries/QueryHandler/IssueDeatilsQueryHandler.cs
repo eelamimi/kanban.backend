b192f80ff4be92bc187f878abe6241a817b3e536
glebka <b.gleba77@gmail.com>
2026-04-21 21:14:13 +0300

    refactor: query

--- Backend.Application/Queries/QueryHandler/IssueDeatilsQueryHandler.cs
+++ Backend.Application/Queries/QueryHandler/IssueDeatilsQueryHandler.cs
@@ -5,10 +5,10 @@ public class IssueDeatilsQueryHandler(
     IAttachmentRepository attachmentRepository)
     : ICommandHandler<IssueDeatilsQuery, IssueResponse>
 {
-    public async Task<IssueResponse> Handle(IssueDeatilsQuery command, CancellationToken token)
+    public async Task<IssueResponse> Handle(IssueDeatilsQuery query, CancellationToken token)
     {
-        var numberInProject = int.Parse(command.PublicId.Split('-', 1)[1]);
-        var issue = await issueRepository.GetByNumberInProjectAndProjectIdsAsync(numberInProject, command.ProjectId, true, token);
+        var numberInProject = int.Parse(query.PublicId.Split('-', 1)[1]);
+        var issue = await issueRepository.GetByNumberInProjectAndProjectIdsAsync(numberInProject, query.ProjectId, true, token);
         var attachments = await attachmentRepository.GetAllByIssueIdAsync(issue.Id, token);
 
         return issue.Map(attachments);
