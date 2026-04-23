168e46e029cc4706cad0fdbfe280edeb06154afe
glebka <b.gleba77@gmail.com>
2026-04-23 21:29:08 +0300

    create attachments command

---
+++ Backend.Application/Commands/Command/Attachment/CreateAttachmentsCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace Backend.Application.Commands.Command;
+
+public class CreateAttachmentsCommand : ICommand
+{
+    public Guid IssueId { get; init; }
+
+    public Guid CommentaryId { get; init; }
+
+    public bool Save { get; init; } = true;
+
+    public IEnumerable<IFormFile> Files { get; init; } = [];
+}
---
+++ Backend.Application/Commands/CommandHandler/Attachment/CreateAttachmentsCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace Backend.Application.Commands.CommandHandler;
+
+public class CreateAttachmentsCommandHandler(
+    IMediator mediator,
+    IAttachmentRepository attachmentRepository)
+    : ICommandHandler<CreateAttachmentsCommand>
+{
+    public async Task Handle(CreateAttachmentsCommand command, CancellationToken token)
+    {
+        foreach (var file in command.Files)
+        {
+            await mediator.Send(new CreateAttachmentCommand
+            {
+                IssueId = command.IssueId,
+                CommentaryId = command.CommentaryId,
+                File = file,
+                Save = false,
+            }, token);
+        }
+
+        if (command.Save)
+            await attachmentRepository.SaveChangesAsync(token);
+    }
+}
