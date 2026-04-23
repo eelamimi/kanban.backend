3323e87753701fabd51174bac6394957d04ebcab
glebka <b.gleba77@gmail.com>
2026-04-23 21:28:08 +0300

    create attachment command

---
+++ Backend.Application/Commands/Command/Attachment/CreateAttachmentCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace Backend.Application.Commands.Command;
+
+public class CreateAttachmentCommand : ICommand
+{
+    public Guid IssueId { get; init; }
+
+    public Guid CommentaryId { get; init; }
+
+    public bool Save { get; init; } = true;
+
+    public IFormFile File { get; init; }
+}
---
+++ Backend.Application/Commands/CommandHandler/Attachment/CreateAttachmentCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿namespace Backend.Application.Commands.CommandHandler;
+
+public class CreateAttachmentCommandHandler(
+    IAttachmentRepository attachmentRepository)
+    : ICommandHandler<CreateAttachmentCommand>
+{
+    public async Task Handle(CreateAttachmentCommand command, CancellationToken token)
+    {
+        var file = command.File;
+        using var memoryStream = new MemoryStream();
+        await file.CopyToAsync(memoryStream, token);
+        var fileBytes = memoryStream.ToArray();
+
+        var attachment = new Attachment
+        {
+            IssueId = command.IssueId,
+            CommentaryId = command.CommentaryId,
+            Content = fileBytes,
+            ContentType = file.ContentType,
+            FileName = file.FileName,
+            Size = file.Length
+        };
+
+        attachmentRepository.Add(attachment);
+
+        if (command.Save)
+            await attachmentRepository.SaveChangesAsync(token);
+    }
+}
