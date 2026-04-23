6842878149236d9dbfb928378ed6c9b44c3e8d09
glebka <b.gleba77@gmail.com>
2026-04-23 21:29:50 +0300

    create commentary with attachments

---
+++ Backend.Application/Commands/Command/Commentary/CreateCommentaryWithAttachmentsCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace Backend.Application.Commands.Command;
+
+public class CreateCommentaryWithAttachmentsCommand : ICommand<IssueResponse>
+{
+    public Guid IssueId { get; init; }
+
+    public Guid AuthorId { get; init; }
+
+    public string Content { get; init; } = string.Empty;
+
+    public IEnumerable<IFormFile> Files { get; init; } = [];
+}
---
+++ Backend.Application/Commands/CommandHandler/Commentary/CreateCommentaryWithAttachmentsCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿namespace Backend.Application.Commands.CommandHandler;
+
+public class CreateCommentaryWithAttachmentsCommandHandler(
+    IMediator mediator,
+    ICommentaryRepository commentaryRepository)
+    : ICommandHandler<CreateCommentaryWithAttachmentsCommand, IssueResponse>
+{
+    public async Task<IssueResponse> Handle(CreateCommentaryWithAttachmentsCommand command, CancellationToken token)
+    {
+        var commentary = new Commentary
+        {
+            IssueId = command.IssueId,
+            AuthorId = command.AuthorId,
+            Content = command.Content,
+            CreatedAt = DateTime.UtcNow,
+        };
+
+        commentaryRepository.Add(commentary);
+
+        await mediator.Send(new CreateAttachmentsCommand
+        {
+            IssueId = command.IssueId,
+            CommentaryId = commentary.Id,
+            Files = command.Files,
+            Save = true,
+        }, token);
+
+        return await mediator.Send(new IssueDeatilsQuery
+        {
+            IssueId = command.IssueId,
+        }, token);
+    }
+}
