3c8887166895273209d8a363e7273f68f9f249d3
glebka <b.gleba77@gmail.com>
2026-04-26 15:38:46 +0300

    update commentary content command

---
+++ Backend.Application/Commands/Command/Commentary/UpdateCommentaryContentCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace Backend.Application.Commands.Command;
+
+public class UpdateCommentaryContentCommand : ICommand<CommentaryResponse>
+{
+    public Guid Id { get; init; }
+
+    public Guid UserProfileId { get; set; }
+
+    public string Content { get; init; } = string.Empty;
+}
