66824164b2b47c573e999705838d39011c2661fe
glebka <b.gleba77@gmail.com>
2026-04-26 15:44:50 +0300

    update commentary content command handler

---
+++ Backend.Application/Commands/CommandHandler/Commentary/UpdateCommentaryContentCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace Backend.Application.Commands.CommandHandler;
+
+public class UpdateCommentaryContentCommandHandler(
+    ICommentaryRepository commentaryRepository)
+    : ICommandHandler<UpdateCommentaryContentCommand, CommentaryResponse>
+{
+    public async Task<CommentaryResponse> Handle(UpdateCommentaryContentCommand command, CancellationToken token)
+    {
+        var commentary = await commentaryRepository.GetByIdAsync(command.Id, token);
+
+        if (commentary.AuthorId != command.UserProfileId)
+            throw new ForbiddenException("Пользователь не является автором комментария");
+
+        commentary.Content = command.Content;
+        commentary.LastEditedAt = DateTime.UtcNow;
+
+        commentaryRepository.Update(commentary);
+        await commentaryRepository.SaveChangesAsync(token);
+
+        return commentary.Map();
+    }
+}
