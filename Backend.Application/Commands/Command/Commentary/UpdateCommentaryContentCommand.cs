f228f70e43a1c82eb949002c1ce09a9bf70077ac
glebka <b.gleba77@gmail.com>
2026-04-26 15:45:10 +0300

    refactor: init user profile id

--- Backend.Application/Commands/Command/Commentary/UpdateCommentaryContentCommand.cs
+++ Backend.Application/Commands/Command/Commentary/UpdateCommentaryContentCommand.cs
@@ -4,7 +4,7 @@ public class UpdateCommentaryContentCommand : ICommand<CommentaryResponse>
 {
     public Guid Id { get; init; }
 
-    public Guid UserProfileId { get; set; }
+    public Guid UserProfileId { get; init; }
 
     public string Content { get; init; } = string.Empty;
 }
