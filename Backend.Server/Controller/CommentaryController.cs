515e8ac812e4daacd93b4c5fd5a708959b35a0de
glebka <b.gleba77@gmail.com>
2026-04-26 15:49:20 +0300

    commentary controller

---
+++ Backend.Server/Controller/CommentaryController.cs
@@ -0,0 +1,14 @@
+﻿namespace Backend.Server.Controller;
+
+[Authorize]
+[Route("api/commentaries")]
+[ApiController]
+public class CommentaryController(IMediator mediator) : ControllerBase
+{
+    [HttpPost]
+    [Route("updateContent")]
+    public async Task<CommentaryResponse> UpdateContent([FromBody] UpdateCommentaryContentCommand command)
+    {
+        return await mediator.Send(command);
+    }
+}
