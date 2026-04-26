f4a90322be223aa2158aa1157cebece628b44179
glebka <b.gleba77@gmail.com>
2026-04-26 11:47:05 +0300

    get attachment content query

---
+++ Backend.Application/Queries/Query/AttachmentContentQuery.cs
@@ -0,0 +1,10 @@
+﻿namespace Backend.Application.Queries.Query;
+
+public class AttachmentContentQuery : ICommand<byte[]>
+{
+    public Guid UserProfileId { get; init; }
+
+    public Guid ProjectId { get; init; }
+
+    public Guid AttachmentId { get; init; }
+}
