c39b028baed62ea9ed7542a8859174bfeaae8522
glebka <b.gleba77@gmail.com>
2026-04-26 11:47:10 +0300

    get attachment content query handler

---
+++ Backend.Application/Queries/QueryHandler/AttachmentContentQueryHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace Backend.Application.Queries.QueryHandler;
+
+public class AttachmentContentQueryHandler(
+    IAttachmentRepository attachmentRepository,
+    ITeamUserProfileRepository teamUserProfileRepository)
+    : ICommandHandler<AttachmentContentQuery, byte[]>
+{
+    public async Task<byte[]> Handle(AttachmentContentQuery query, CancellationToken token)
+    {
+        if (!await teamUserProfileRepository.IsInProject(query.UserProfileId, query.ProjectId, token))
+            throw new ForbiddenException("User is not in project");
+
+        return await attachmentRepository.GetContentByIdAsync(query.AttachmentId, token);
+    }
+}
