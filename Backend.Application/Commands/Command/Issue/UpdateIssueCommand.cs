9acad14a4c57da65f9fd42ad40427a0bf6e46610
glebka <b.gleba77@gmail.com>
2026-04-27 22:27:47 +0300

    command of update issue

---
+++ Backend.Application/Commands/Command/Issue/UpdateIssueCommand.cs
@@ -0,0 +1,22 @@
+﻿namespace Backend.Application.Commands.Command;
+
+public class UpdateIssueCommand : ICommand<IssueResponse>
+{
+    public Guid Id { get; init; }
+
+    public Guid AssigneeId { get; init; }
+
+    public Guid AuthorId { get; init; }
+
+    public string Title { get; init; } = string.Empty;
+
+    public IssueType IssueType { get; init; }
+
+    public int StoryPoints { get; init; }
+
+    public IssuePriority IssuePriority { get; init; }
+
+    public string Description { get; init; } = string.Empty;
+
+    public List<IFormFile> Files { get; set; } = [];
+}
