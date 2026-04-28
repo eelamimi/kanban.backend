7440013c06130c3cda8eeec7d04db034596b9e86
glebka <b.gleba77@gmail.com>
2026-04-28 09:42:03 +0300

    file storage service interface

---
+++ Backend.Domain/Service/IFileStorageService.cs
@@ -0,0 +1,12 @@
+﻿namespace Backend.Domain.Service;
+
+public interface IFileStorageService
+{
+    Task<string> SaveFileAsync(byte[] content, string fileName, string contentType, CancellationToken ct = default);
+
+    Task<byte[]> GetFileAsync(string filePath, CancellationToken ct = default);
+
+    Task DeleteFileAsync(string filePath, CancellationToken ct = default);
+
+    string GetStorageRootPath();
+}
