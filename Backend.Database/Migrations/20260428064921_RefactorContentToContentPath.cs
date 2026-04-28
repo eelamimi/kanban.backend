b6e4faa72df8678bf8965a81582a2348228aac09
glebka <b.gleba77@gmail.com>
2026-04-28 09:58:37 +0300

    remove content from db

---
+++ Backend.Database/Migrations/20260428064921_RefactorContentToContentPath.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefactorContentToContentPath : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Content",
+                table: "Attachment");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContentPath",
+                table: "Attachment",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContentPath",
+                table: "Attachment");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Content",
+                table: "Attachment",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+    }
+}
