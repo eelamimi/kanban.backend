6258ebb089c55c3654f9404dec8e6bb71a8100d6
glebka <b.gleba77@gmail.com>
2026-04-30 19:33:11 +0300

    build origin

--- Backend.Server/Program.cs
+++ Backend.Server/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy(frontendName, policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:5173", "http://localhost:4173")
               .AllowAnyMethod()
               .AllowAnyHeader()
               .AllowCredentials();
