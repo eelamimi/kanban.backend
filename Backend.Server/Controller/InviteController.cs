c69bac9e65e91e5b09df957c6f51c8cf60806146
glebka <b.gleba77@gmail.com>
2026-05-01 11:45:16 +0300

    invite controller

---
+++ Backend.Server/Controller/InviteController.cs
@@ -0,0 +1,13 @@
+﻿namespace Backend.Server.Controller;
+
+[Authorize]
+[Route("api/invites")]
+[ApiController]
+public class InviteController(IMediator mediator) : ControllerBase
+{
+    [HttpPost]
+    public async Task<string> CreateInvite([FromForm] GetOrCreateInviteCommand command)
+    {
+        return await mediator.Send(command);
+    }
+}
