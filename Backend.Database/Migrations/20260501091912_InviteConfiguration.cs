4a35d3db6da018341a74a9f6679ebbd405bf7ca8
glebka <b.gleba77@gmail.com>
2026-05-01 12:25:52 +0300

    invite configuration migration

---
+++ Backend.Database/Migrations/20260501091912_InviteConfiguration.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class InviteConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Invite",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    TeamId = table.Column<Guid>(type: "uuid", nullable: false),
+                    RoleId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Token = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ExpiresAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Invite", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Invite_Role_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Role",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Invite_Team_TeamId",
+                        column: x => x.TeamId,
+                        principalTable: "Team",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX__Invite__ExpiresAt",
+                table: "Invite",
+                column: "ExpiresAt");
+
+            migrationBuilder.CreateIndex(
+                name: "IX__Invite__RoleId",
+                table: "Invite",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX__Invite__TeamId",
+                table: "Invite",
+                column: "TeamId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX__Invite__Token",
+                table: "Invite",
+                column: "Token",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Invite");
+        }
+    }
+}
