9a8d26de2aa30911b9b0b4ba6dc9a9be1ebe3292
glebka <b.gleba77@gmail.com>
2026-05-01 10:13:51 +0300

    create invite command

---
+++ Backend.Application/Commands/Command/Invite/CreateInviteCommand.cs
@@ -0,0 +1,6 @@
+﻿namespace Backend.Application.Commands.Command;
+
+public class CreateInviteCommand : ICommand<string>
+{
+    public Guid TeamId { get; init; }
+}
---
+++ Backend.Application/Commands/CommandHandler/Invite/CreateInviteCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace Backend.Application.Commands.CommandHandler;
+
+public class CreateInviteCommandHandler(
+    ITokenService tokenService,
+    IInviteRepository inviteRepository):
+    ICommandHandler<CreateInviteCommand, string>
+{
+    public async Task<string> Handle(CreateInviteCommand command, CancellationToken token)
+    {
+        var inviteToken = tokenService.GenerateToken();
+        var createdAt = DateTime.UtcNow;
+
+        var invite = new Invite
+        {
+            TeamId = command.TeamId,
+            Token = inviteToken,
+            CreatedAt = createdAt,
+            ExpiresAt = createdAt.AddDays(1),
+        };
+
+        inviteRepository.Add(invite);
+        await inviteRepository.SaveChangesAsync(token);
+
+        return inviteToken;
+    }
+}
