089e9eeccdfee144590306af4959f0f4632c6871
glebka <b.gleba77@gmail.com>
2026-05-01 19:15:01 +0300

    update user role command

---
+++ Backend.Application/Commands/Command/Role/UpdateUserRoleCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace Backend.Application.Commands.Command;
+
+public class UpdateUserRoleCommand : ICommand<UserRolePair>
+{
+    public Guid UserProfileId { get; init; }
+
+    public Guid RoleId { get; init; }
+}
---
+++ Backend.Application/Commands/CommandHandler/Role/UpdateUserRoleCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿namespace Backend.Application.Commands.CommandHandler;
+
+public class UpdateUserRoleCommandHandler(
+    IRoleRepository roleRepository,
+    ITeamUserProfileRepository teamUserProfileRepository) :
+    ICommandHandler<UpdateUserRoleCommand, UserRolePair>
+{
+    public async Task<UserRolePair> Handle(UpdateUserRoleCommand command, CancellationToken token)
+    {
+        var newRole = await roleRepository.GetByIdAsync(command.RoleId, token);
+
+        if (!await teamUserProfileRepository.IsInTeam(command.UserProfileId, newRole.TeamId, token))
+            throw new ForbiddenException("Пользователь не является участником команды");
+
+        var teamUserProfile = await teamUserProfileRepository.GetByUserProfileAndTeamIdAsync(
+            command.UserProfileId, newRole.TeamId, true, false, false, token);
+
+        teamUserProfile.Role = newRole;
+        teamUserProfile.RoleId = newRole.Id;
+
+        teamUserProfileRepository.Update(teamUserProfile);
+        await teamUserProfileRepository.SaveChangesAsync(token);
+
+        return new UserRolePair
+        {
+            Role = newRole.Map(),
+            User = teamUserProfile.UserProfile.Map(),
+        };
+    }
+}
