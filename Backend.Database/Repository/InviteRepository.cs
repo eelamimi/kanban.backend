c57d918e9a8ef8acfcbda075347e2dc4a940e9ad
glebka <b.gleba77@gmail.com>
2026-05-01 10:12:49 +0300

    invite repository

---
+++ Backend.Database/Repository/InviteRepository.cs
@@ -0,0 +1,35 @@
+﻿namespace Backend.Database.Repository;
+
+public class InviteRepository(ApplicationDbContext context) : IInviteRepository
+{
+    public async Task<Invite> GetByIdAsync(Guid id, CancellationToken token = default)
+    {
+        return await TryGetByIdAsync(id, token) 
+            ?? throw new NullReferenceException("Invite not found");
+    }
+
+    public async Task<Invite?> TryGetByIdAsync(Guid id, CancellationToken token = default)
+    {
+        return await context.Invites.FirstOrDefaultAsync(x => x.Id == id, token);
+    }
+
+    public async void Add(Invite invite)
+    {
+        context.Add(invite);
+    }
+
+    public async void Update(Invite invite)
+    {
+        context.Update(invite);
+    }
+
+    public async void Remove(Invite invite)
+    {
+        context.Remove(invite);
+    }
+
+    public async Task<int> SaveChangesAsync(CancellationToken token = default)
+    {
+        return await context.SaveChangesAsync(token);
+    }
+}
---
+++ Backend.Domain/Repository/IInviteRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace Backend.Domain.Repository;
+
+public interface IInviteRepository
+{
+    Task<Invite> GetByIdAsync(Guid id, CancellationToken token = default);
+
+    Task<Invite?> TryGetByIdAsync(Guid id, CancellationToken token = default);
+
+    void Add(Invite invite);
+
+    void Update(Invite invite);
+
+    void Remove(Invite invite);
+
+    Task<int> SaveChangesAsync(CancellationToken token = default);
+}
