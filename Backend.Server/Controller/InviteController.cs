b3bf207aa05f7486bb4994422ef964c9f0898a67
glebka <b.gleba77@gmail.com>
2026-05-01 11:55:08 +0300

    command is from body

--- Backend.Server/Controller/InviteController.cs
+++ Backend.Server/Controller/InviteController.cs
@@ -6,7 +6,7 @@
 public class InviteController(IMediator mediator) : ControllerBase
 {
     [HttpPost]
-    public async Task<string> CreateInvite([FromForm] GetOrCreateInviteCommand command)
+    public async Task<string> CreateInvite([FromBody] GetOrCreateInviteCommand command)
     {
         return await mediator.Send(command);
     }
