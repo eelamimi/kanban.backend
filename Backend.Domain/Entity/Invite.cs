3562f5108c8cacddd3e2b1a74aaee72c45e733c8
glebka <b.gleba77@gmail.com>
2026-05-01 10:12:34 +0300

    invite entity

---
+++ Backend.Domain/Entity/Invite.cs
@@ -0,0 +1,14 @@
+﻿namespace Backend.Domain.Entity;
+
+public class Invite : BaseEntity
+{
+    public Guid TeamId { get; set; }
+
+    public Team Team { get; set; }
+
+    public string Token { get; set;  } = string.Empty;
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime ExpiresAt { get; set; }
+}
