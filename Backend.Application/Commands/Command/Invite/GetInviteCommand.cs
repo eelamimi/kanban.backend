1f17e9676118fca2d477a20b394a8ad364387985
glebka <b.gleba77@gmail.com>
2026-05-01 10:27:19 +0300

    get invite command

---
+++ Backend.Application/Commands/Command/Invite/GetInviteCommand.cs
@@ -0,0 +1,6 @@
+﻿namespace Backend.Application.Commands.Command;
+
+public class GetInviteCommand : ICommand<string?>
+{
+    public Guid TeamId { get; init; }
+}
---
+++ Backend.Application/Commands/CommandHandler/Invite/GetInviteCommandHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace Backend.Application.Commands.CommandHandler;
+
+public class GetInviteCommandHandler(
+    IInviteRepository inviteRepository) :
+    ICommandHandler<GetInviteCommand, string?>
+{
+    public async Task<string?> Handle(GetInviteCommand command, CancellationToken token)
+    {
+        return await inviteRepository.GetLastByTeamIdAsync(command.TeamId, token);
+    }
+}
