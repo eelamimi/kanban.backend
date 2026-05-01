fe9e89ab456ea3394303edbc9173bd57763b4c5b
glebka <b.gleba77@gmail.com>
2026-05-01 10:36:44 +0300

    get or create command

---
+++ Backend.Application/Commands/Command/Invite/GetOrCreateInviteCommand.cs
@@ -0,0 +1,6 @@
+﻿namespace Backend.Application.Commands.Command;
+
+public class GetOrCreateInviteCommand : ICommand<string>
+{
+    public Guid TeamId { get; init; }
+}
---
+++ Backend.Application/Commands/CommandHandler/Invite/GetOrCreateInviteCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace Backend.Application.Commands.CommandHandler;
+
+public class GetOrCreateInviteCommandHandler(
+    IMediator mediator) :
+    ICommandHandler<GetOrCreateInviteCommand, string>
+{
+    public async Task<string> Handle(GetOrCreateInviteCommand command, CancellationToken token)
+    {
+        var inviteToken = await mediator.Send(new GetActiveInviteQuery
+        {
+            TeamId = command.TeamId,
+        }, token);
+
+        if (inviteToken == null)
+        {
+            return await mediator.Send(new CreateInviteCommand
+            {
+                TeamId = command.TeamId
+            }, token);
+        }
+
+        return inviteToken;
+    }
+}
