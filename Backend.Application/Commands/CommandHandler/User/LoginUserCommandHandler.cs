0fcd79c1c8ee5fc90c3d5621855110b081d4600c
glebka <b.gleba77@gmail.com>
2026-05-13 01:25:24 +0300

    en -> ru exception messages

--- Backend.Application/Commands/CommandHandler/User/LoginUserCommandHandler.cs
+++ Backend.Application/Commands/CommandHandler/User/LoginUserCommandHandler.cs
@@ -9,10 +9,10 @@ public class LoginUserCommandHandler(
     public async Task<RegistryOrLoginUserResponse> Handle(LoginUserCommand command, CancellationToken token)
     {
         var user = await userRepository.TryGetByEmailAsync(command.Email, true, token)
-            ?? throw new UserInputException($"User with email {command.Email} does not exist");
+            ?? throw new UserInputException($"Пользователя с почтой {command.Email} не существует");
 
         if (!passwordHasher.VerifyPassword(command.Password, user.Password))
-            throw new UserInputException("Incorrect password");
+            throw new UserInputException("Неверный пароль, попробуйте ещё раз");
 
         var userProfile = user.UserProfile;
         var jwtToken = jwtService.GenerateToken(user.Id, userProfile.FirstName, userProfile.SecondName, user.Email);
